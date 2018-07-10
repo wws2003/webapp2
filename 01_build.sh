87a61fcb84f7455c788240b55f54dde891a4e923
Trung Pham Tien <wws2003@gmail.com>
2018-07-10 09:02:31 +0700

    Minor changes (tested on windows)

---
+++ 01_build.sh
@@ -0,0 +1,6 @@
+# Install web-common
+mvn clean install -pl web-common -am
+
+# Package (later will be integration-test to deploy)
+# Note: Package goal only is desirable, but actually it only overlay web-common-lib.jar, so war:war must be added as a workaround
+mvn clean package war:war
