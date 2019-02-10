3ddd0c9c19ac623480200cdf530988366db087d5
wws2003 <wws2003@gmail.com>
2019-02-10 21:57:05 +0700

    Managed to have the deferredresult work in Spring sec by directly dealing with HttpServletResponse

--- web-project/src/main/java/org/hpg/project/biz/web/document/ExperimentDocController.java
+++ web-project/src/main/java/org/hpg/project/biz/web/document/ExperimentDocController.java
@@ -5,8 +5,17 @@
  */
 package org.hpg.project.biz.web.document;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.PrintWriter;
 import java.util.concurrent.Callable;
+import javax.servlet.AsyncContext;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.hpg.common.biz.service.abstr.ILogger;
 import org.hpg.common.biz.service.abstr.ITaskExecutor;
+import org.hpg.common.model.dto.web.AjaxResult;
+import org.hpg.common.util.AjaxResultBuilder;
 import org.hpg.project.constant.ProjectUrls;
 import org.hpg.project.dao.repository.es.IEsDocumentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,20 +40,78 @@ public class ExperimentDocController {
     @Autowired
     private ITaskExecutor taskExecutor;
 
+    @Autowired
+    private ILogger logger;
+
     @GetMapping("/testTest")
     @ResponseBody
     public String testTest() {
         return "1234";
     }
 
-    @GetMapping("/testIndex")
-    @ResponseBody
-    public DeferredResult<String> testIndex() {
-        DeferredResult<String> ret = new DeferredResult(100L);
+    @GetMapping("/testIndex1")
+    public void testIndexPureServlet(HttpServletRequest request, HttpServletResponse response) {
+        // Create context for an async search
+        AsyncContext asyncContext = request.startAsync(request, response);
+
         taskExecutor.submit("12345678", () -> {
             try {
+                Thread.sleep(1200);
+                ServletResponse servResponse = asyncContext.getResponse();
+                response.setContentType("application/json");
+                response.setCharacterEncoding("UTF-8");
+
+                AjaxResult ajaxResult = AjaxResultBuilder.successInstance()
+                        .resultObject("1234")
+                        .oneSuccessMessage("Index success")
+                        .build();
+
+                ObjectMapper objMapper = new ObjectMapper();
+
+                try (PrintWriter writer = servResponse.getWriter()) {
+                    writer.write(objMapper.writeValueAsString(ajaxResult));
+                    writer.flush();
+                } catch (Exception ex) {
+                    logger.error(ex);
+                }
+            } catch (InterruptedException e) {
+                logger.error(e);
+            }
+        });
+
+        // Return immediately
+    }
+
+    @GetMapping("/testIndex")
+    @ResponseBody
+    public DeferredResult<String> testIndex(HttpServletRequest request, HttpServletResponse response) {
+        // Two minute timeout for the request
+        DeferredResult<String> ret = new DeferredResult(2 * 60 * 1000L);
+        taskExecutor.submit("12345678", () -> {
+            try {
+                Thread.sleep(1200);
+                ret.setResultHandler((Object result) -> {
+
+                    response.setContentType("application/json");
+                    response.setCharacterEncoding("UTF-8");
+
+                    AjaxResult ajaxResult = AjaxResultBuilder.successInstance()
+                            .resultObject(result)
+                            .oneSuccessMessage("Index success")
+                            .build();
+
+                    ObjectMapper objMapper = new ObjectMapper();
+
+                    try (PrintWriter writer = response.getWriter()) {
+                        writer.write(objMapper.writeValueAsString(ajaxResult));
+                        writer.flush();
+                    } catch (Exception ex) {
+                        logger.error(ex);
+                    }
+                });
                 ret.setResult("1234");
-            } catch (Exception e) {
+            } catch (InterruptedException e) {
+                logger.error(e);
                 ret.setErrorResult(e);
             }
         });
